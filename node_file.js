fe8fffe9ef24c15efa722722f78131342f1e0224
Sanshi <1204807266@qq.com>
2019-05-05 17:58:47 +0800

    Update node_file.js

--- node_file.js
+++ node_file.js
@@ -1,6 +1,8 @@
 require('shelljs/global');
+var utilBuild = require('./pythonTool/utilBuild');
 var fs = require('fs');
 var path = require('path')
+var adm_zip = require('adm-zip');
 let channelId = process.argv[2];
 if(!channelId){
     console.error('请传入一个正确的渠道id 打单个渠道 传1 ');
@@ -92,56 +94,88 @@ function mkdirsSync(dirname) {
 //  channelId => 渠道ID
 //  file => 文件名
 //  dataPath => 文件路径
-function createConfig(channelId, file, dataPath){
+//  state => 是否使用渠道id 0:1
+function createConfig(channelId, file, dataPath, code=0){
 
     if(!channelId || !file || !dataPath){
         console.error('迁移失败-》',dataPath+file);
         return false;
     }
     mkdirsSync(dataPath);
-    let Path = `${file} ${dataPath}`;
+    //修改log文件名
+    if(file == 'appCfg/android/res/mipmap-mdpi/ic_launcher.png'){
+        dataPath+= 'logo.png';
+    }
+    if(code == 1){
+        var Path = `./${channelId}/${file} ${dataPath}`;
+        }else{
+            var Path = `${file} ${dataPath}`;
+    }
     let state = exec(`cp -f ${Path}`);
     if(state == 0){
-        console.info('迁移成功-》',dataPath+file);
+        console.info('迁移成功-》',dataPath+'<--'+file);
         return true;
     }
-    console.error('迁移失败-》',dataPath+file);
+    console.error('迁移失败-》',dataPath+'<--'+file);
     return false;
 }
 
-var config ={
-    './defaultCfg/platform/config.php' : `./output/platform/application/`,
-    //配置文件，redis/充值地址...
-    './defaultCfg/platform/database.php' : `./output/platform/application/`,
-    //数据库配置文件
-    './defaultCfg/admin/config.php' : `./output/admin/application/`,
-    './defaultCfg/admin/database.php' : `./output/admin/application/`
+//  打包ZIP
+function zipFolder(folder, outFolder) {
+    let obj = new adm_zip();
+    obj.addLocalFolder(folder);
+    obj.writeZip(`${outFolder}`);
+    console.error(`${outFolder}`);
+    delete obj;
 };
-var config_com =[
-    "appCfg/android/res/mipmap-mdpi/ic_launcher.png" , `./output/platform/public/static/agent/images/`
+
+
+//当前时间
+let time = new Date();
+var prefix = './output/'+channelId+'_'+time.toLocaleDateString()+'-'+time.getHours()+'-'+time.getMinutes();
+//复制项目
+fs.mkdirSync(prefix);
+exec(`cp -fr ./output/platformS ${prefix}/platform`);
+exec(`cp -fr ./output/adminS ${prefix}/admin`);
+//删除git
+exec(`rm -rf ${prefix}/admin/.git`);
+exec(`rm -rf ${prefix}/platform/.git`);
+var config ={
+    './defaultCfg/platform/config.php' : `${prefix}/platform/application/`,
+    //配置文件，redis/充值地址...
+    './defaultCfg/platform/database.php' : `${prefix}/platform/application/`,
+    //数据库配置文件
+    './defaultCfg/admin/config.php' : `${prefix}/admin/application/`,
+    './defaultCfg/admin/database.php' : `${prefix}/admin/application/`
+};
+var config_com ={
+    "appCfg/android/res/mipmap-mdpi/ic_launcher.png" : `${prefix}/platform/public/static/agent/images/`,
     //二维码中间的icon：logo.png  48*48
-];
+    "channelConfig.json" : `${prefix}/admin/application/`,
+    "showSwicthConfig.json" : `${prefix}/admin/application/`
+    //不同渠道的配置文件
+};
 
 let count = 0;
-//单独迁移图标
-mkdirsSync(config_com[1]);
-let png_state = exec(`cp -r -f './'${channelId}/${config_com[0]} ${config_com[1]}`);
-if(png_state == 0){
-    console.info('迁移成功-》',config_com[0]);
-    count++;
-}else{
-    console.error('迁移失败-》',config_com[0]);
-}
-
 let config_arr = Object.keys(config);
 let conf_len = config_arr.length;
-//循环处理配置文件（覆盖成模板文件)
+let config_com_arr = Object.keys(config_com);
+let conf_com_len = config_com_arr.length;
+//循环处理模板配置文件
     for(let x=0; x< conf_len; ++x){
-        let state = createConfig(channelId,config_arr[x],config[config_arr[x]]);
+        let state = createConfig(channelId,config_arr[x],config[config_arr[x]],0);
             if(!!state){
                 count++;
             }
     }
-    console.warn('\n渠道=>'+channelId+'\n迁移成功数=>',count);
-    //替换配置参数
- //   modifyValue();
+    for(let x=0; x< conf_com_len; ++x){
+        let state = createConfig(channelId,config_com_arr[x],config_com[config_com_arr[x]],1);
+            if(!!state){
+                count++;
+            }
+    }
+console.warn('\n渠道=>'+channelId+'\n迁移成功数=>',count);
+//替换配置参数
+//   modifyValue();
+//   打包ZIP
+ zipFolder(prefix,prefix+'Zip.zip');
